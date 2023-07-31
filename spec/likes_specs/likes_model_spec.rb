b0ed19f26de3ac9576fbdfbd8c8e400574ea5350
Ihor Fedorchenko <ihor.fedorchenko@evowill.com>
2023-07-31 14:15:20 +0300

    Pull request fixes

--- spec/likes_specs/likes_model_spec.rb
+++ spec/likes_specs/likes_model_spec.rb
@@ -1,13 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
+RSpec.describe Like, type: :model do
   describe 'Associations' do
     it { should belong_to(:user) }
-    it { should belong_to(:post) }
-  end
-
-  describe 'Validations' do
-    it { should validate_presence_of(:text) }
-    it { should validate_length_of(:text).is_at_most(100) }
+    it { should belong_to(:likeable) }
   end
 end
