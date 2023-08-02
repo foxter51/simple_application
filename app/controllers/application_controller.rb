4ea894c9f1d9af111b827f077f406fec7de03f16
Ihor Fedorchenko <ihor.fedorchenko@evowill.com>
2023-08-02 11:17:31 +0300

    Add policy and roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # Represents base application controller
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
   def index; end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:warning] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,25 @@
 # Represents User model
 class User < ApplicationRecord
+  rolify
+  after_create :set_default_role, :set_admin_if_first
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes
 
   has_one_attached :avatar
+
+  def set_default_role
+    add_role(:blogger) if roles.blank?
+  end
+
+  def set_admin_if_first
+    add_role(:admin) if User.count == 1
+  end
+
+  def add_another_role(role)
+    add_role(role) unless has_role? role
+  end
 
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
