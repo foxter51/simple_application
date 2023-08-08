3f75464ea3188330dcdca492d1add75354259e45
Ihor Fedorchenko <ihor.fedorchenko@evowill.com>
2023-08-08 14:28:54 +0300

    Logger improvement

--- app/sidekiq/letter_job.rb
+++ app/sidekiq/letter_job.rb
@@ -4,9 +4,13 @@ class LetterJob
 
   def perform
     Rails.logger.info "Starting job of sending top10 daily"
-    User.find_each do |user|
-      @posts = user.subscribed_posts.top(10)
-      SubscriptionMailer.top10_posts_by_clock(user, @posts).deliver_later if @posts.any?
+    begin
+      User.find_each do |user|
+        @posts = user.subscribed_posts.top(10)
+        SubscriptionMailer.top10_posts_by_clock(user, @posts).deliver_later if @posts.any?
+      end
+    rescue StandardError => e
+      Rails.logger.error "Error sending letter: #{e.message}"
     end
     Rails.logger.info "Finishing job of sending top10 daily"
   end
